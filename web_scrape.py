86404b32c0e0fd1c328b0b9c5f4af514faa3ed00
Jean-Ezra Yeung <jyeung@Jean-Ezras-MacBook-Pro.local>
2016-04-12 23:38:36 -0400

    Iterate website download

--- web_scrape.py
+++ web_scrape.py
@@ -1,55 +1,31 @@
-# https://automatetheboringstuff.com/chapter11/
-
-# http://docs.python-guide.org/en/latest/scenarios/scrape/
-
-from lxml import html
 import requests
 
-page = requests.get('http://www.spine-health.com/forum/discussion/90448/pain/lower-back-pain/running-out-options')
-tree = html.fromstring(page.content)
-vanilla = tree.xpath('//*[@id="Discussion_90448"]/div/div[2]/div/div[1]/text()[2]')
-print vanilla
+# Download file
+def download_website(website,file_name):
+    page = requests.get(website)
+    try:
+        page.raise_for_status()
+    except Exception as exc:
+        print('There was a problem: %s' % (exc))
+    file = open(file_name,'wb')
+    for chunk in page.iter_content(100000):
+        file.write(chunk)
+    file.close()
 
-page2 = requests.get('http://www.spine-health.com/forum/discussion/90489/pain/lower-back-pain/chronic-lower-back-pain-after-pregnancy')
-tree2 = html.fromstring(page2.content)
-preg = tree2.xpath('//*[@id="Discussion_90489"]/div/div[2]/div/div[1]/text()')
-print preg
 
-page3 = requests.get('http://www.spine-health.com/forum/discussion/44887/pain/lower-back-pain/back-pain-worse-and-different-after-cortisone-injection')
-tree3 = html.fromstring(page3.content)
-surg = tree3.xpath('//*[@id="Discussion_44887"]/div/div[2]/div/div[1]/text()[2]')
-print surg
+download_website('http://www.spine-health.com/forum/discussion/90643/pain/lower-back-pain/l5s1-when-consider-surgery','link1.txt')
 
-page4 = requests.get('http://www.spine-health.com/forum/discussion/90441/pain/lower-back-pain/when-do-you-consider-surgery')
-tree4 = html.fromstring(page4.content)
-cons = tree4.xpath('//*[@id="Discussion_90441"]/div/div[2]/div/div[1]/text()[1]')
-print cons
-cons2 = tree4.xpath('//*[@id="Discussion_90441"]/div/div[2]/div/div[1]/text()[2]')
-print cons2
+websites = ['http://www.spine-health.com/forum/discussion/90643/pain/lower-back-pain/l5s1-when-consider-surgery',
+            'http://www.spine-health.com/forum/discussion/90590/pain/lower-back-pain/normal-scans-still-pretty-severe-pain',
+            'http://www.spine-health.com/forum/discussion/90632/pain/lower-back-pain/pain-extending-flexing',
+            'http://www.spine-health.com/forum/discussion/90640/pain/lower-back-pain/synovial-cyst-l4-l5-bulge-pain-anyone-want-chat-future-coping',
+            'http://www.spine-health.com/forum/discussion/90626/pain/lower-back-pain/degenerative-osteoarthritis-after-car-crash'
+            ]
 
-num = range(1,7)
+num = range(1,6)
+file_names = ['link' + str(c) + '.txt' for c in num]
+print file_names
 
-corpus = []
-for i in num:
-    text = 'cons' + str(i)
-    corpus.append(text)
-
-print corpus
-
-forrest = []
-for i in num:
-    wood = 'tree' + str(i)
-    forrest.append(wood)
-
-print forest
-
-element = []
-for i in num:
-    atom = """//*[@id="Discussion_90441"]/div/div[2]/div/div[1]/text()[""" + str(i) + ']'
-    element.append(atom)
-
-print element
-
-dict = {}
-for i,j in zip(corpus,element):
-    dict[i] = tree4.xpath(j)
+for i,j in zip(websites,file_names):
+    download_website(i,j)
+    print j
