49cc1871b1afe949fd370775bbdcc9f6ed6c02e3
Jean-Ezra Yeung <jyeung@Jean-Ezras-MacBook-Pro.local>
2016-04-21 23:14:58 -0400

    Scrape data elements

--- web_scrape.py
+++ web_scrape.py
@@ -1,6 +1,7 @@
-from lxml import html
 import requests
 import bs4
+
+
 
 # FUNCTIONS
 def download_website(website,file_name):
@@ -28,19 +29,6 @@ def site_iterator(websites,file_names):
         print 'Created ', j
 
 
-def main(websites,prefix,num_files):
-    file_names = naming_links(prefix,num_files)
-    site_iterator(websites,file_names)
-    print 'Downloaded websites'
-
-
-response = requests.get('http://www.spine-health.com/forum/categories/lower-back-pain')
-soup = bs4.BeautifulSoup(response.text)
-# links = soup.select('#Body a[href^=http://www.spine-health.com/forum/discussion]')
-links = [a.attrs.get('href') for a in soup.select('#Body a[href^=http://www.spine-health.com/forum/discussion]')]
-print links
-print len(links)
-
 def unique_links(input):
     output = []
     for x in input:
@@ -48,40 +36,52 @@ def unique_links(input):
             output.append(x)
     return output
 
+
+def main(websites,prefix,num_files):
+    file_names = naming_links(prefix,num_files)
+    site_iterator(websites,file_names)
+    print 'Downloaded websites'
+
+
+def scrape_element(prefix,element):
+    messages = soup.select(element)
+    length = len(messages)
+    print length
+    n = range(0,length)
+    print n
+    dict = {}
+    for i in n:
+        dict[prefix+str(i)] = soup.select(element)[i].get_text()
+    return dict
+
+
+# Scraping weblinks
+response = requests.get('http://www.spine-health.com/forum/categories/lower-back-pain')
+soup = bs4.BeautifulSoup(response.text)
+links = [a.attrs.get('href') for a in soup.select('#Body a[href^=http://www.spine-health.com/forum/discussion]')]
+print links
+print len(links)
+
+# Find unique links
 links_unq = unique_links(links)
 print len(links_unq)
-
 matching = [s for s in links_unq if "#latest" in s]
 len(matching)
-main(matching,'lbp',30)
+main(matching,'lbp',len(links_unq))
 
-def get_patient_data(patient_page_url):
-    response = requests.get('http://www.spine-health.com/forum/discussion/77790/pain/lower-back-pain/lumbar-spine-locked#latest')
-    soup = bs4.BeautifulSoup(response.text)
-    patient_data = {}
-    patient_data['message'] = soup.select('.Message')[0].get_text()
-    patient_data['title'] = soup.select('.Username')[0].get_text()
-    patient_data['post_cnt'] = soup.select('.MItem.PostCount')[0].get_text()
-    patient_data['time'] = soup.select('.time')[0].get_text()
-
-    patient_data['speakers'] = [a.get_text() for a in soup.select('div#sidebar a[href^=/speaker]')]
-    patient_data['youtube_url'] = soup.select('div#sidebar a[href^=http://www.youtube.com]')[0].get_text()
+# Scraping pages
+response = requests.get('http://www.spine-health.com/forum/discussion/77790/pain/lower-back-pain/lumbar-spine-locked#latest')
+soup = bs4.BeautifulSoup(response.text)
+# Metadata
+forum = scrape_element('forum','.MItem.Category')
+title = soup.find('div',class_='PageTitle').h1
+# Data
+messages = scrape_element('messages','.Message')
+posts_cnt = scrape_element('posts_cnt','.MItem.PostCount')
+created = scrape_element('created','.MItem.DateCreated')
+updated = scrape_element('updated','.DateUpdated')
+author = scrape_element('author','.Author')
 
 # MAIN CODE
 # if __name__="__main__":
 #     main(websites)
-# Back pain
-websites = ['http://www.spine-health.com/forum/discussion/90643/pain/lower-back-pain/l5s1-when-consider-surgery',
-            'http://www.spine-health.com/forum/discussion/90590/pain/lower-back-pain/normal-scans-still-pretty-severe-pain',
-            'http://www.spine-health.com/forum/discussion/90632/pain/lower-back-pain/pain-extending-flexing',
-            'http://www.spine-health.com/forum/discussion/90640/pain/lower-back-pain/synovial-cyst-l4-l5-bulge-pain-anyone-want-chat-future-coping',
-            'http://www.spine-health.com/forum/discussion/90626/pain/lower-back-pain/degenerative-osteoarthritis-after-car-crash'
-            ]
-
-# Pregnancy - complications
-# Source: http://www.whattoexpect.com/forums/complications.html
-websites = ['http://www.whattoexpect.com/forums/complications/topic/abnormal-labs.html',
-            'http://www.whattoexpect.com/forums/complications/topic/13-week-u-s-left-side-of-the-heart-is-smaller-any-similar-experiences.html',
-            'http://www.whattoexpect.com/forums/complications/topic/baby-is-here-3285.html',
-            'http://www.whattoexpect.com/forums/complications/topic/cerclage-and-still-bleeding-help.html',
-            'http://www.whattoexpect.com/forums/complications/topic/toxoplasmosis-question-48.html']
