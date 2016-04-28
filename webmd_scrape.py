1aba7cf988f4ca0b5592ab9e9aabe5e12e0bd221
Jean-Ezra Yeung <jyeung@Jean-Ezras-MacBook-Pro.local>
2016-04-27 23:50:16 -0400

    Scrape WebMD

--- webmd_scrape.py
+++ webmd_scrape.py
@@ -107,8 +107,7 @@ def create_date_diff(df_create, var, prefix):
     return df_create_describe
 
 
-def posts_create_data(soup):
-    dates = scrape_element(soup, 'dates', '.posted_fmt')
+def posts_create_clean(dates):
     create_dates2 =[]
     for k, v in dates.items():
         create_dates2.append(v)
@@ -121,10 +120,39 @@ def posts_create_data(soup):
     df_create_dates2['date'] = df_create_dates2['dt'].apply(lambda x: pd.to_datetime(x))
     df_create_dates2.sort_values('date',ascending=True,inplace=True)
     df_create_dates2.reset_index(inplace=True)
-    date_size = len(df_create_dates2)
-    df_next = df_create_dates2['date'].ix[1:date_size - 1].reset_index()
+    return df_create_dates2
+
+
+def posts_first_create(date):
+    create_date = []
+    create_date.append(date['dates0'])
+    df_create_date = pd.DataFrame(create_date)
+    df_create_date[1] = df_create_date[0].apply(lambda x: x.strip('\r\n\t'))
+    df_create_date[2] = df_create_date[1].apply(lambda x: x.lstrip('Last Reply: '))
+    df_create_date[3] = df_create_date[2].apply(lambda x: x.strip('\r\n\t '))
+    df_create_date[4] = df_create_date[3].apply(lambda x: x.strip('\n\t'))
+    df_create_date[5] = df_create_date[4].apply(lambda x: x.strip("document.write(DateDelta('"))
+    df_create_date[6] = df_create_date[5].apply(lambda x: x.replace("GMT-0400","").replace("(EDT)",""))
+    df_create_date[7] = df_create_date[6].apply(lambda x: x.replace("GMT-0500","").replace("(EST)",""))
+    df_create_date['dt'] = df_create_date[7].apply(lambda x: x.rstrip(');'))
+    df_create_date['date'] = df_create_date['dt'].apply(lambda x: pd.to_datetime(x))
+    return df_create_date
+
+
+def posts_create_data(soup):
+    date = scrape_element(soup, 'dates', '.first_posted_fmt')
+    dates = scrape_element(soup, 'dates', '.posted_fmt')
+    df_create_date = posts_first_create(date)
+    df_create_dates2 = posts_create_clean(dates)
+    df_create_dates3 = pd.concat([df_create_date[['date']],df_create_dates2[['date']]],axis=0)
+    df_create_dates3.reset_index(inplace=True)
+    df_create_dates3 = df_create_dates3[['date']].sort_values('date',ascending=True)
+    df_create_dates3.reset_index(inplace=True)
+    df_create_dates3 = df_create_dates3[['date']].sort_values('date', ascending=True)
+    date_size = len(df_create_dates3)
+    df_next = df_create_dates3['date'].ix[1:date_size - 1].reset_index()
     df_next.columns = ['index', 'next_date']
-    df_now_next = DataFrame(df_create_dates2['date']).join(DataFrame(df_next['next_date']))
+    df_now_next = DataFrame(df_create_dates3['date']).join(DataFrame(df_next['next_date']))
     df_now_next['diff'] = (df_now_next['next_date'] - df_now_next['date']) / np.timedelta64(1, 'D')
     df_date = create_date_diff(df_now_next, 'date', 'create_').reset_index().drop('index', 1)
     df_diff = create_date_diff(df_now_next, 'diff', 'datediff_').reset_index().drop('index', 1).astype(float)
@@ -142,20 +170,20 @@ def main():
     forum_link = 'http://exchanges.webmd.com/back-pain-exchange'
     web_links = scrape_links(forum_link)
     matching = scrape_unique_links(web_links)
-    i = 1
-    for m in matching:
-        print m, '\n', i, '\n', datetime.now()
-        response = requests.get(m)
-        soup = bs4.BeautifulSoup(response.text)
-        df_title = title_data(soup)
-        df_msg = messages_data(soup)
-        df_date = posts_create_data(soup)
-        all_data = combine_data(df_title, df_msg, df_posts, df_date)
-        if i == 1:
-            all_data.to_csv('back_pain.csv', delimiter=',', header=True, index=True, mode='w')
-        else:
-            all_data.to_csv('back_pain.csv', delimiter=',', header=False, index=True, mode='a')
-        i += 1
+i = 1
+for m in matching:
+    print m, '\n', i, '\n', datetime.now()
+    response = requests.get(m)
+    soup = bs4.BeautifulSoup(response.text)
+    df_title = title_data(soup)
+    df_msg = messages_data(soup)
+    df_date = posts_create_data(soup)
+    all_data = combine_data(df_title, df_msg, df_date)
+    if i == 1:
+        all_data.to_csv('back_pain.csv', delimiter=',', header=True, index=True, mode='w')
+    else:
+        all_data.to_csv('back_pain.csv', delimiter=',', header=False, index=True, mode='a')
+    i += 1
 
 
 # MAIN CODE
